6fdcae984cb525690fc960518bbc6dd0670aceaf
xyz2781790037 <2781790037@qq.com>
2025-03-25 22:40:13 +0800

    newfile

--- udp/client.cc
+++ udp/client.cc
@@ -4,9 +4,32 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string>
+#include "th_pool.hpp"
 #define SERVER_IP "127.0.0.1" // 服务器IP地址
 #define SERVER_PORT 8080      // 服务器端口
 #define BUFFER_SIZE 1024      // 缓冲区大小
+bool runflag = true;
+struct ThreadArgs
+{
+    int sockfd;
+    struct sockaddr_in server;
+};
+void *receive(void *arg)
+{
+    char buffer[BUFFER_SIZE];
+    ThreadArgs *sock = static_cast<ThreadArgs *>(arg);
+    socklen_t addr_len = sizeof(sock->server);
+    while (runflag)
+    {
+        ssize_t recv_len = recvfrom(sock->sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&sock->server, &addr_len);
+        buffer[recv_len] = '\0';
+        if (recv_len > 0)
+        {
+            std::cout << "收到服务器回复: " << buffer << std::endl;
+        }
+    }
+    return nullptr;
+}
 int main()
 {
     int sockfd;
@@ -21,20 +44,18 @@ int main()
     server_addr.sin_port = htons(SERVER_PORT);
     server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
     std::cout << "UDP 客户端已启动，输入消息并按 Enter 发送..." << std::endl;
+    ThreadArgs th = {sockfd, server_addr};
+    pool po;
+    po.enqueue(receive, &th);
     while (1)
     {
         std::cout << "输入消息: ";
-        getline(std::cin,buffer);
+        getline(std::cin, buffer);
         sendto(sockfd, buffer.c_str(), buffer.size(), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
-        if (buffer=="exit")
+        if (buffer == "exit")
         {
             std::cout << "客户端退出..." << std::endl;
             break;
-        }
-        socklen_t addr_len = sizeof(server_addr);
-        ssize_t recv_len = recvfrom(sockfd, (void *)buffer.c_str(), BUFFER_SIZE, 0, (struct sockaddr *)&server_addr, &addr_len);
-        if(recv_len > 0){
-            std::cout << "收到服务器回复: " << buffer << std::endl;
         }
     }
     close(sockfd);
