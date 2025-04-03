02b10f8ec9aaedb3b57b1bc5cf4a3959d210ff28
xyz2781790037 <2781790037@qq.com>
2025-04-03 22:06:30 +0800

    newfile

--- ftp/server.cc
+++ ftp/server.cc
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/sendfile.h>
 #define PORT 2100
+#define BUFFER_MAXSIZE 1024
 void set_blocking(int fd){
     int flags = fcntl(fd, F_GETFL, 0);
     fcntl(fd, F_SETFL, flags | O_NONBLOCK);
@@ -17,6 +20,38 @@ int main(){
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = PORT;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    
+    if(bind(server_fd,(struct sockaddr*)&server_addr,sizeof(server_addr)) < 0){
+        perror("bind");
+        close(server_fd);
+        exit(1);
+    }
+    if(listen(server_fd,100) < 0){
+        perror("listen");
+        close(server_fd);
+        exit(1);
+    }
+    set_blocking(server_fd);
+    std::cout << "listen : " << PORT << std::endl;
+    while(true){
+        struct sockaddr_in client_addr;
+        socklen_t sock_len = sizeof(client_addr);
+        int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &sock_len);
+        if(client_fd < 0){
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+                continue;
+            perror("accept");
+            continue;
+        }
+        set_blocking(client_fd);
+        std::cout << "Connecting from :" << client_fd << std::endl;
+        char buffer[BUFFER_MAXSIZE];
+        int read_bytes = read(client_fd, buffer, BUFFER_MAXSIZE);
+        if(read_bytes < 0){
+            perror("read");
+            close(client_fd);
+            exit(1);
+        }
+        // sendfile()
+    }
     return 0;
 }
